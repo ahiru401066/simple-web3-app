c1e7150a0dd6a1857a4301a92a19e1bf84d0cc83
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-02-21 00:04:55 +0900

    フロントとバックのコンテナ接続完了

--- frontend/sample-app/src/App.tsx
+++ frontend/sample-app/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const API_URL = "http://localhost:8000"; 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState<string>("");
+  useEffect(() => {
+    fetch(`${API_URL}/`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  useEffect(() => {
+    console.log(message);  // message が更新されるたびにログを表示
+  }, [message]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+      <h2>{message}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          カウント回数： {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
